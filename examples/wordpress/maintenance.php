394089a3807d15e74bdb63459daf2d3747e8e091
Yaro WebDev <yg@think.studio>
2023-10-05 18:44:08 +0300

    Added maintenance template

---
+++ examples/wordpress/maintenance.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+    body { text-align: center; padding: 60px; }
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; background-color: #0a112e; color: #fff; }
+    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+    a { color: #ffc608; text-decoration: none; transition: all 0.5s; }
+    a:hover { color: #539d0a; text-decoration: none; }
+</style>
+
+<article>
+    <img src="https://raw.githubusercontent.com/dev-think-one/branding/556ad75bc55a892aeb1302479a03abea83745f42/assets/studio/logo.svg" style="width: 80px;">
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. We&rsquo;ll be back online shortly!</p>
+        <p>&mdash; <a target="_blank" href="https://think.studio">The Development Team </a></p>
+    </div>
+</article>
+
+<script type="text/javascript">
+    setTimeout(function (){window.location="/";}, 30*1000);
+</script>
